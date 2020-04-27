96f92f0f60683862fe0c3c368fd82330ac2087f6
Dahwi Kim <ekgnlqlem@gmail.com>
2020-04-27 20:54:48 +0900

    created auth.js & contacts.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.get("/", (req, res) => res.json({ msg: "Welcome to the PhoneBook API" }));
 app.use("/api/users", require("./routes/users"));
 app.use("/api/contacts", require("./routes/contacts"));
 app.use("/api/auth", require("./routes/auth"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
