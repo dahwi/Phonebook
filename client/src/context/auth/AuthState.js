2c1b5ba05d16dcba46f2da5b641659e1fa9fc723
Dahwi Kim <ekgnlqlem@gmail.com>
2020-05-07 00:00:07 +0900

    Auth Context & Initial State

---
+++ client/src/context/auth/AuthState.js
@@ -0,0 +1,53 @@
+import AuthContext from "./authContext";
+import React, { useReducer } from "react";
+import authReducer from "./authReducer";
+import {
+  REMOVE_ALERT,
+  CONTACT_ERROR,
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+  CLEAR_ERRORS,
+} from "../types";
+
+const AuthState = (props) => {
+  const initialState = {
+    token: localStorage.getItem("token"),
+    isAuthenticated: null,
+    loading: true,
+    error: null,
+    user: null,
+  };
+
+  const [state, dispatch] = useReducer(authReducer, initialState);
+
+  //Load User
+
+  //Register a User
+
+  //Login User
+
+  //Logout
+
+  //Clear Errors
+
+  return (
+    <AuthContext.Provider
+      value={{
+        token: state.token,
+        isAuthenticated: state.isAuthenticated,
+        loading: state.loading,
+        error: state.error,
+        user: state.user,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthState;
